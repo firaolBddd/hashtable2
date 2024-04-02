cbce22a089f265101257637dcfc56bce0ff8f633
Firaol Berehanu <firaolberehanu@MacBook-Pro-79.local>
2024-04-02 01:20:16 -0700

    part 2

--- main.cpp
+++ main.cpp
@@ -5,5 +5,23 @@
 #include <fstream>
 
 using namespace std;
+// Struct definition
+struct Student {
+    char givenName[100];
+    char surname[100];
+    float grade;
+    int studentID;
+    Student* next = NULL;
+};
+
+// Function declarations
+Student** addPupils(Student** roster, int &currentID, int &capacity);
+Student** manAdd(Student** roster, int &currentID, int &capacity);
+void displayAll(Student** roster, int capacity);
+void display(Student** roster, int capacity);
+void eliminate(Student** roster, int capacity);
+void resetRoster(Student* roster[], int capacity);
+void printChain(Student* current);
+Student** rehashRoster(Student** roster, Student** newRoster, int &capacity, int currentID);
 
 
